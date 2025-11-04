6f12fbe90bee1d6538fd116a671df40661ce6973
Natalia Montserrat Cavazos Almeida <165847114+natslayer3@users.noreply.github.com>
2025-11-04 16:20:42 -0600

    subir archivos

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:latest
+
+WORKDIR /app
+
+COPY . .
+RUN pip install Flask
+
+EXPOSE 3000
+
+CMD ["python", "app.py"]
---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return jsonify({"message": "it works! Student A01571724"})
+
+if __name__ == "__main__":
+	app.run(threaded=True, host='0.0.0.0', port=3000)
